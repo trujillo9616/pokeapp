5e583f1dfe452af65eb7fad9574d5f84355b8a9b
Adrian Trujillo <adrian.td96@gmail.com>
2022-10-26 17:07:09 -0500

    added pokemon sprites and names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 import { trpc } from "../utils/trpc";
@@ -15,6 +16,13 @@ const Home: NextPage = () => {
     setSecond(second);
   }, []);
 
+  const firstPokemon = trpc.getPokemonById.useQuery({ id: first });
+  const secondPokemon = trpc.getPokemonById.useQuery({ id: second });
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <Head>
@@ -26,9 +34,37 @@ const Home: NextPage = () => {
       <div className="text-2xl text-center">Which Pokémon is cuter?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-400">{first}</div>
+        <div className="w-64 h-64 flex flex-col justify-center items-center">
+          <Image
+            src={firstPokemon.data?.sprites.front_default!}
+            alt={firstPokemon.data?.name!}
+            width={64}
+            height={64}
+            placeholder="blur"
+            blurDataURL={firstPokemon.data?.sprites.front_default!}
+            priority
+            className="w-full h-full"
+          />
+          <div className="text-2xl text-center capitalize mt-[-0.5rem]">
+            {firstPokemon.data?.name}
+          </div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-pink-400">{second}</div>
+        <div className="w-64 h-64 flex flex-col justify-center items-center">
+          <Image
+            src={secondPokemon.data?.sprites.front_default!}
+            alt={secondPokemon.data?.name!}
+            width={64}
+            height={64}
+            placeholder="blur"
+            blurDataURL={secondPokemon.data?.sprites.front_default!}
+            priority
+            className="w-full h-full"
+          />
+          <div className="text-2xl text-center capitalize mt-[-0.5rem]">
+            {secondPokemon.data?.name}
+          </div>
+        </div>
       </div>
     </div>
   );
