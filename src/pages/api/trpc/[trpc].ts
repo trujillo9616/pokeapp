e38bdb31e95906d6ee950ba3b7c47571361a2e09
Adrian Trujillo <adrian.td96@gmail.com>
2022-10-27 10:27:59 -0500

    created query infer type

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,11 @@
+import { inferProcedureInput, inferProcedureOutput } from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
-import { appRouter } from '../../../server/routers/_app';
+import { appRouter, AppRouter } from '../../../server/routers/_app';
 
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => ({}),
 });
+
+export type inferQueryResponse<TRouteKey extends keyof AppRouter['_def']['procedures']> = inferProcedureOutput<AppRouter['_def']['procedures'][TRouteKey]>
