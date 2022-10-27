d5964512ae5dfbf0c907ae6aa1ee6090f2fd850e
Adrian Trujillo <adrian.td96@gmail.com>
2022-10-27 12:24:30 -0500

    added vote mutation

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 import { PokemonClient } from 'pokenode-ts';
+import { prisma } from '../utils/prisma';
 
 export const appRouter = router({
   getPokemonById: publicProcedure
@@ -14,6 +15,21 @@ export const appRouter = router({
       const pokemon = await api.getPokemonById(input.id);
       return { id: pokemon.id, name: pokemon.name, sprites: pokemon.sprites };
     }),
+  castVote: publicProcedure
+    .input(
+      z.object({
+        votedFor: z.number(),
+        votedAgainst: z.number(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const voteInDB = await prisma.vote.create({
+        data: {
+          ...input
+        }
+      })
+      return { success: true, vote: voteInDB };
+    })
 });
 
 // export type definition of API
