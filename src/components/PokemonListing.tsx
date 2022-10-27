ba814e36426c6a5e947061b3fd3c0b42df5c2edc
Adrian Trujillo <adrian.td96@gmail.com>
2022-10-27 10:28:43 -0500

    refactor pokemon listing into its own component

---
+++ src/components/PokemonListing.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { inferQueryResponse } from "@/pages/api/trpc/[trpc]";
+import Image from "next/image";
+
+interface PokemonListingProps {
+  pokemon: inferQueryResponse<"getPokemonById"> | undefined;
+  vote: (selected: number | undefined) => void;
+}
+
+const BUTTON = {
+  string: "Cuter",
+  style:
+    "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded my-2",
+};
+
+const PokemonListing: React.FC<PokemonListingProps> = ({ pokemon, vote }) => {
+  return (
+    <div className="w-64 h-64 flex flex-col justify-center items-center">
+      <Image
+        src={pokemon?.sprites.front_default!}
+        alt={pokemon?.name!}
+        width={64}
+        height={64}
+        placeholder="blur"
+        blurDataURL={pokemon?.sprites.front_default!}
+        priority
+        className="w-full h-full"
+      />
+      <div className="text-2xl text-center capitalize mt-[-0.5rem]">
+        {pokemon?.name}
+      </div>
+      <button className={BUTTON.style} onClick={() => vote(pokemon?.id)}>
+        {BUTTON.string}
+      </button>
+    </div>
+  );
+};
+
+export default PokemonListing;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useState } from "react";
+
+import PokemonListing from "@/components/PokemonListing";
 
 import { trpc } from "../utils/trpc";
 
@@ -19,9 +20,12 @@ const Home: NextPage = () => {
   const firstPokemon = trpc.getPokemonById.useQuery({ id: first });
   const secondPokemon = trpc.getPokemonById.useQuery({ id: second });
 
-  if (firstPokemon.isLoading || secondPokemon.isLoading) {
-    return <div>Loading...</div>;
-  }
+  const voteForCutest = (selected: number | undefined) => {
+    // TODO: implement voting
+    const [first, second] = getOptionsForVote();
+    setFirst(first);
+    setSecond(second);
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
@@ -34,37 +38,15 @@ const Home: NextPage = () => {
       <div className="text-2xl text-center">Which Pokémon is cuter?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-64 h-64 flex flex-col justify-center items-center">
-          <Image
-            src={firstPokemon.data?.sprites.front_default!}
-            alt={firstPokemon.data?.name!}
-            width={64}
-            height={64}
-            placeholder="blur"
-            blurDataURL={firstPokemon.data?.sprites.front_default!}
-            priority
-            className="w-full h-full"
-          />
-          <div className="text-2xl text-center capitalize mt-[-0.5rem]">
-            {firstPokemon.data?.name}
-          </div>
-        </div>
-        <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col justify-center items-center">
-          <Image
-            src={secondPokemon.data?.sprites.front_default!}
-            alt={secondPokemon.data?.name!}
-            width={64}
-            height={64}
-            placeholder="blur"
-            blurDataURL={secondPokemon.data?.sprites.front_default!}
-            priority
-            className="w-full h-full"
-          />
-          <div className="text-2xl text-center capitalize mt-[-0.5rem]">
-            {secondPokemon.data?.name}
-          </div>
-        </div>
+        {!firstPokemon.isLoading && !secondPokemon.isLoading ? (
+          <>
+            <PokemonListing pokemon={firstPokemon.data} vote={voteForCutest} />
+            <div className="p-8">Vs</div>
+            <PokemonListing pokemon={secondPokemon.data} vote={voteForCutest} />
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </div>
   );
