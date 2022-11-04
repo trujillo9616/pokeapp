4a633f87e89a5fe8303825673857dab65aa2238f
Adrian Trujillo <adrian.td96@gmail.com>
2022-11-04 00:13:21 -0600

    added motion

--- src/components/PokemonListing.tsx
+++ src/components/PokemonListing.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { inferQueryResponse } from "@/pages/api/trpc/[trpc]";
 import Image from "next/image";
 
@@ -16,17 +17,34 @@ const BUTTON = {
 const PokemonListing: React.FC<PokemonListingProps> = ({ pokemon, vote }) => {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <Image
-        src={pokemon?.sprites.front_default!}
-        alt={pokemon?.name!}
-        width={64}
-        height={64}
-        priority
+      <motion.div
         className="w-full h-full"
-      />
-      <div className="text-2xl text-center capitalize mt-[-0.5rem]">
-        {pokemon?.name}
-      </div>
+        whileHover={{
+          y: [0, -50, 0, -50, 0],
+          scale: [1, 1.1, 1, 1.1, 1],
+          transition: { duration: 0.8 },
+        }}
+        transition={{ duration: 0.4 }}
+      >
+        <Image
+          src={pokemon?.spriteUrl!}
+          alt={pokemon?.name!}
+          width={64}
+          height={64}
+          priority
+          className="w-full h-full"
+        />
+      </motion.div>
+
+      <a
+        href={`https://bulbapedia.bulbagarden.net/wiki/${pokemon?.name}_(Pok%C3%A9mon)`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="text-2xl text-center capitalize mt-[-0.5rem]">
+          {pokemon?.name}
+        </div>
+      </a>
       <button className={BUTTON.style} onClick={() => vote(pokemon?.id)}>
         {BUTTON.string}
       </button>
